d08e70b9f321c57135b43c5ebe5bbd8c1e4e9f12
Ashish777Dev <ashishvasavafox43@gmail.com>
2026-04-03 13:36:57 +0530

    Fix : solve the backspace issue and highlight text while typing

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let difficultyIndexes = {
   medium: -1,
   hard: -1,
 };
+let characters = [];
+let allSpanElement;
+let letters;
+let charIndex = 0;
 const difficultyButtons = document.querySelectorAll(".difficulty-btn");
 const textSection = document.querySelector(".text-section");
 
@@ -36,12 +40,45 @@ function startGame() {
     let type = e.target.dataset.difficultyType; //EAST MEDIUM HARD
 
     if (typingData[type]) {
+      //difficultyIndexes[type]; easy=-1 , medium=-1 and hard=-1
       difficultyIndexes[type] =
         (difficultyIndexes[type] + 1) % typingData[type].length;
 
       let text = typingData[type][difficultyIndexes[type]].text;
-      let id = typingData[type][difficultyIndexes[type]].id;
-      textSection.innerText = `${text}   ID:${id}`;
+      characters = text.split("");
+
+      letters = characters.map((ch) => `<span>${ch}</span>`).join("");
+
+      textSection.innerHTML = letters;
     }
   }
+
+  document.addEventListener("keydown", (e) => {
+    let ch = e.key;
+
+    if (ch === "Backspace") {
+      if (charIndex > 0) {
+        charIndex--;
+        allSpanElement[charIndex].classList.remove("correct", "in-correct");
+      }
+
+      return;
+    }
+
+    e.preventDefault();
+
+    allSpanElement = textSection.querySelectorAll("span");
+    const isAlphabet = /^[a-zA-Z0-9\s.,!?'";:()\-]$/.test(ch);
+
+    if (charIndex < allSpanElement.length && isAlphabet) {
+      if (ch === allSpanElement[charIndex].innerText) {
+        allSpanElement[charIndex].classList.add("correct");
+        allSpanElement[charIndex].classList.remove("in-correct");
+      } else {
+        allSpanElement[charIndex].classList.add("in-correct");
+        allSpanElement[charIndex].classList.remove("correct");
+      }
+      charIndex++;
+    }
+  });
 }
