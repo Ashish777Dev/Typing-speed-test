321410e8dd78a271136e4c6534927287f284b9df
Ashish777Dev <ashishvasavafox43@gmail.com>
2026-04-14 16:49:58 +0530

    FIX: show default text on first load

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let typingData = [];
 let difficultyIndexes = {
-  easy: 0,
+  easy: -1,
   medium: -1,
   hard: -1,
 };
@@ -14,15 +14,17 @@ const textSection = document.querySelector(".text-section");
 const loadData = async () => {
   try {
     const res = await fetch("./data.json");
-    const data = await res.json();
 
     if (!res.ok) {
       throw new Error("There was a problem while loading data!");
     }
 
+    const data = await res.json();
     typingData = data;
 
-    startGame();
+    initEventListener();
+
+    traverseText("easy");
   } catch (error) {
     throw new Error("Failed to fetch data!");
   }
@@ -30,68 +32,69 @@ const loadData = async () => {
 
 loadData();
 
-function startGame() {
-  textSection.innerHTML = typingData["easy"][0].text;
-
+function initEventListener() {
   difficultyButtons.forEach((difficulty) => {
-    difficulty.addEventListener("click", traverseText);
+    difficulty.addEventListener("click", (e) => {
+      //EAST MEDIUM HARD
+      traverseText(e.target.dataset.difficultyType);
+    });
   });
 
-  function traverseText(e) {
-    let type = e.target.dataset.difficultyType; //EAST MEDIUM HARD
+  document.addEventListener("keydown", handleTyping);
+}
 
-    if (typingData[type]) {
-      //charIndex = 0; reset index after each text
-      charIndex = 0;
+function traverseText(type) {
+  if (typingData[type]) {
+    //charIndex = 0; reset index after each text
+    charIndex = 0;
 
-      //difficultyIndexes[type]; easy=-1 , medium=-1 and hard=-1
-      difficultyIndexes[type] =
-        (difficultyIndexes[type] + 1) % typingData[type].length;
+    //difficultyIndexes[type]; easy=-1 , medium=-1 and hard=-1
+    difficultyIndexes[type] =
+      (difficultyIndexes[type] + 1) % typingData[type].length;
 
-      let text = typingData[type][difficultyIndexes[type]].text;
-      let id = typingData[type][difficultyIndexes[type]].id;
-      characters = text.split("");
+    let text = typingData[type][difficultyIndexes[type]].text;
+    let id = typingData[type][difficultyIndexes[type]].id;
+    characters = text.split("");
 
-      letters = characters.map((ch) => `<span>${ch}</span>`).join("");
+    letters = characters.map((ch) => `<span>${ch}</span>`).join("");
 
-      textSection.innerHTML = `${letters} ID:${id}`;
+    textSection.innerHTML = `${letters} ID:${id}`;
+  }
+}
+
+function handleTyping(e) {
+  let ch = e.key;
+
+  allSpanElement = textSection.querySelectorAll("span");
+
+  allSpanElement.forEach((span) => span.classList.remove("caret"));
+
+  if (ch === "Backspace") {
+    if (charIndex > 0) {
+      charIndex--;
+      allSpanElement[charIndex].classList.remove("correct", "in-correct");
+      allSpanElement[charIndex].classList.add("caret");
     }
+
+    return;
   }
 
-  document.addEventListener("keydown", (e) => {
-    let ch = e.key;
+  e.preventDefault();
 
-    allSpanElement = textSection.querySelectorAll("span");
+  const isAlphabet = /^[a-zA-Z0-9\s.,!?'";:()\-]$/.test(ch);
 
-    allSpanElement.forEach((span) => span.classList.remove("caret"));
+  if (charIndex < allSpanElement.length) {
+    allSpanElement[charIndex + 1].classList.add("caret");
+  }
 
-    if (ch === "Backspace") {
-      if (charIndex > 0) {
-        charIndex--;
-        allSpanElement[charIndex].classList.remove("correct", "in-correct");
-        allSpanElement[charIndex].classList.add("caret");
-      }
-
-      return;
+  if (charIndex < allSpanElement.length && isAlphabet) {
+    if (ch === allSpanElement[charIndex].innerText) {
+      allSpanElement[charIndex].classList.add("correct");
+      allSpanElement[charIndex].classList.remove("in-correct");
+    } else {
+      allSpanElement[charIndex].classList.add("in-correct");
+      allSpanElement[charIndex].classList.remove("correct");
     }
-
-    e.preventDefault();
-
-    const isAlphabet = /^[a-zA-Z0-9\s.,!?'";:()\-]$/.test(ch);
-
-    if (charIndex < allSpanElement.length) {
-      allSpanElement[charIndex + 1].classList.add("caret");
-    }
-
-    if (charIndex < allSpanElement.length && isAlphabet) {
-      if (ch === allSpanElement[charIndex].innerText) {
-        allSpanElement[charIndex].classList.add("correct");
-        allSpanElement[charIndex].classList.remove("in-correct");
-      } else {
-        allSpanElement[charIndex].classList.add("in-correct");
-        allSpanElement[charIndex].classList.remove("correct");
-      }
-      charIndex++;
-    }
-  });
+    charIndex++;
+  }
 }
